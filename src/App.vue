aea8c056d82b3bac980f3484cff5836d18794450
Nickolsky <romb.intu@gmail.com>
2022-05-31 16:55:13 +0300

    0.1.1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
+    <nav-bar />
     <h2>NOTES</h2>
-    <div v-if="notes == []">
-        <ul>
-            <li v-for="note in notes" :key="note.id">{{note.Title}}</li>
-        </ul>
+    <div v-if="notes != []">
+        <div id="notes" v-for="note in notes" :key="note.id">
+            <div>{{note.title}}</div>
+            <div>Последнее изменение: {{moment(note.updated_at).fromNow()}}</div>
+            <a href="{{note/id/note._id}}">Подробнее</a>
+        </div>
     </div>
 
     <div v-else>Notes not found.</div>
 </template>
 
 <script>
-// import IndexNotes from './components/IndexNotes.vue'
+import NavBar from './components/NavBar.vue'
 import axios from 'axios'
+import moment from 'moment'
 
+moment.locale("ru")
 const host = "http://172.18.9.6:8081"
 
 export default {
   name: 'App',
   components: {
-   // IndexNotes,
+    NavBar,
   },
   data() {
     return {
@@ -33,10 +38,23 @@ export default {
         }).catch((err) => {
             console.log("ERROR", err)
         })
+  },
+  methods: {
+    moment
   }
 }
 </script>
 
 <style>
-
+body {
+    background: #e4f68f;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+#notes {
+    margin: 5%;
+    border-radius: 2%;
+    background-color: #50c19a;
+}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="topnav">
+        <a href="#home">SANOTE</a>
+        <a href="#news">GitHub</a>
+        <a href="#contact">Регистрация</a>
+        <a href="#about">Вход</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar",    
+}
+</script>
+
+
+<style>
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #686354;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #50c19a;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #50c19a;
+  color: white;
+}
+</style>
